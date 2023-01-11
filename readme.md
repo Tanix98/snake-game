9ba954651153f051b2bd03852ca833761914ea8a
Tanix <91118560+Tanix98@users.noreply.github.com>
2023-01-11 13:41:18 +0100

    add script for the snake body to follow the snake head, prevent apple from spawning in snake body or wall, increase movement speed each time apple is eaten, add grey wall, make the code clearer by separating it into several functions

--- readme.md
+++ readme.md
@@ -1,5 +1,15 @@
 # Snake game
 
-A simple snake game made using HTML and JavaScript.
+## A simple snake game made using HTML and JavaScript. [Click here to play](https://google.com)
 
-[Click here to play](https://google.com)
+### Potential future features
+- Grey walls with randomized sizes and spawn locations scattered throughout the map. Will result in game over if snake hits them
+- Different colored snake head
+- Snake able to turn backwards if no apples have been eaten
+- Sound effects
+- Background image
+
+### Known bugs:
+- Every third game started the snake will increase by two instead of one when eating the first apple
+- Snake cannot collide with the last part of its body
+- Grey wall only spawns as one block
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const playerScore = document.querySelector("#score-number");
 const playerHighscore = document.querySelector("#highscore-number");
 const resetScoreBtn = document.querySelector("#reset-score-btn");
+/*let score = 0;*/
 
 if (localStorage.getItem("Highscore")) {
     playerHighscore.innerHTML = localStorage.getItem("Highscore");
@@ -10,171 +11,221 @@ if (localStorage.getItem("Highscore")) {
 
 let snake = {
     unit: 25,
-    y: 0,
     x: 0,
+    y: 0,
     directionX: 0,
     directionY: 0,
-    color: "white",
+    color: "green",
     bodyX: [],
     bodyY: []
 }
 
 let apple = {
     unit: 25,
+    x: 0,
     y: 0,
-    x: 0
+    color: "red"
 }
-apple.y = parseInt(Math.random() * 12) * apple.unit,
-apple.x = parseInt(Math.random() * 12) * apple.unit
+apple.x = parseInt(Math.random() * 24) * apple.unit,
+apple.y = parseInt(Math.random() * 24) * apple.unit
 
-ctx.fillRect(snake.x, snake.y, snake.unit, snake.unit);
+let wall = {
+    unit: 25,
+    x: 0,
+    y: 0,
+    color: "grey",
+    bodyX: [375,350,320],
+    bodyY: [525,525,525]
+}
+wall.x = parseInt(Math.random() * 24) * wall.unit,
+wall.y = parseInt(Math.random() * 24) * wall.unit
 
 document.addEventListener("keydown", move);
 
 function move(e) {
+    // Moves snake with arrowkeys
     switch (e.key) {
         case "ArrowLeft":
-            snake.directionX = -1;
-            snake.directionY = 0;
-            break;
+        case "a":
+            if (!snake.directionX) {
+                snake.directionX = -1;
+                snake.directionY = 0;
+            }
+            e.preventDefault();
+        break;
         case "ArrowUp":
-            snake.directionX = 0;
-            snake.directionY = -1;
+        case "w":
+            if (!snake.directionY) {
+                snake.directionX = 0;
+                snake.directionY = -1;
+            }
             e.preventDefault();
-            break;
+        break;
         case "ArrowRight":
-            snake.directionX = 1;
-            snake.directionY = 0;
-            break;
-        case "ArrowDown":
-            snake.directionX = 0;
-            snake.directionY = 1;
+        case "d":
+            if (!snake.directionX) {
+                snake.directionX = 1;
+                snake.directionY = 0;
+            }
             e.preventDefault();
-            break;
+        break;
+        case "ArrowDown":
+        case "s":
+            if (!snake.directionY) {
+                snake.directionX = 0;
+                snake.directionY = 1;
+            }
+            e.preventDefault();
+        break;
         default: 
-            break;
+        break;
     }
 }
 
-setInterval(draw, 150);
+let frameSkips = 10;
+let count = 0;
 
-function draw() {
-    if (snake.y < 0 || snake.y > 575 || snake.x < 0 || snake.x > 575) {
-        snake.x = 0;
-        snake.y = 0;
-        snake.directionX = 0;
-        snake.directionY = 0;
-        apple.y = parseInt(Math.random() * 12) * snake.unit;
-        apple.x = parseInt(Math.random() * 12) * snake.unit;
-        snake.bodyX = [];
-        snake.bodyY = [];
-        if (Number(playerScore.innerHTML) > Number(playerHighscore.innerHTML)) {
-            alert("New High Score! ツ");
-        } else {
-            alert("Game Over! o︵o");
-        }
-        console.log(playerScore.innerHTML + " omg " +  playerHighscore.innerHTML);
-        if (Number(playerScore.innerHTML) > Number(playerHighscore.innerHTML)) {
-            localStorage.setItem("Highscore", playerScore.innerHTML);
-        }
-        if (localStorage.getItem("Highscore")) {
-            playerHighscore.innerHTML = localStorage.getItem("Highscore");
-        }
-        playerScore.innerHTML = 0;
+setInterval(gameLoop, 25);
+
+function gameLoop() {
+    if (count >= frameSkips) {
+        game();
+        count = 0;
     } else {
+        count++;
+    }
+}
+
+function game() {
+    // Check if snake hits apple
+    eat();
+    if (snake.y < 0 || snake.y > 575 || snake.x < 0 || snake.x > 575 || snake.x === wall.x && snake.y === wall.y) {
+        gameOver();
+    }
+    else {
         snake.x += snake.directionX * snake.unit;
         snake.y += snake.directionY * snake.unit;
     }
+    if (collisionCheck()) {
+        gameOver();
+    }
+    draw();
+}
+
+function draw() {
     ctx.clearRect(0, 0, 600, 600);
-    // Check if snake hits apple
-    eat();
-    // Snake head
-    ctx.fillStyle = "green";
+    // Draw walls
+    ctx.fillStyle = wall.color;
+    ctx.fillRect(wall.x, wall.y, wall.unit, wall.unit);
+    // Draw apple
+    ctx.fillStyle = apple.color;
+    ctx.fillRect(apple.x, apple.y, apple.unit, apple.unit);
+    // Draw snake head
+    ctx.fillStyle = snake.color;
     ctx.fillRect(snake.x, snake.y, snake.unit, snake.unit);
     // Draw snake body
     for (let i = 0; i < snake.bodyX.length; i++) {
         ctx.fillRect(snake.bodyX[i], snake.bodyY[i], snake.unit, snake.unit)
     }
-    // Apple
-    ctx.fillStyle = "red";
-    ctx.fillRect(apple.x, apple.y, apple.unit, apple.unit);
 }
 
 function eat() {
+    // Eat apple
     if (snake.x === apple.x && snake.y === apple.y) {
-        apple.y = parseInt(Math.random() * 12) * apple.unit;
-        apple.x = parseInt(Math.random() * 12) * apple.unit;
+        /*score++;*/
         playerScore.innerHTML++;
         grow();
+        if (frameSkips > 2.5) {
+            frameSkips--;
+            console.log("speed");
+        }
+        do {
+            apple.y = parseInt(Math.random() * 24) * apple.unit;
+            apple.x = parseInt(Math.random() * 24) * apple.unit;
+        } while (spawnCheck());
     } else {
         moveBody();
     }
 }
 
 function grow() {
+    // Grow snake body
     snake.bodyX = [snake.x].concat(snake.bodyX);
     snake.bodyY = [snake.y].concat(snake.bodyY);
-    console.log("x: " + snake.bodyX);
-    console.log("y: " + snake.bodyY);
-    console.log(" ");
+    console.log("x: " + snake.bodyX + " " + "y: " + snake.bodyY);
 }
 
 function moveBody() {
-    for (let i = snake.bodyX.length; i > snake.bodyX.length; i--) {
-        snake.bodyX[i] = snake.bodyX[i-1];
-        snake.bodyY[i] = snake.bodyY[i-1];
+    // Move snake body
+    if (snake.bodyX.length > 1) {
+        for (let i = snake.bodyX.length - 1; i > 0; i--) {
+            snake.bodyX[i] = snake.bodyX[i-1];
+            snake.bodyY[i] = snake.bodyY[i-1];
+        }
     }
-    if (snake.bodyX > 0) {
+    if (snake.bodyX.length > 0) {
         snake.bodyX[0] = snake.x;
         snake.bodyY[0] = snake.y;
     }
 }
 
-resetScoreBtn.addEventListener('click', (e) => {
-    localStorage.removeItem("Highscore", playerScore.innerHTML);
-    location.reload();
+function collisionCheck() {
+    // Checks if snake hits itself
+    for (let i = 0; i < snake.bodyX.length; i++) {
+        if (snake.bodyX[i] === snake.x && snake.bodyY[i] === snake.y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function spawnCheck() {
+    // Check if the apple spawns in snake body or a wall
+    for (let i = 0; i < snake.bodyX.length; i++) {
+        if (snake.bodyX[i] === apple.x && snake.bodyY[i] === apple.y || apple.x === wall.x && apple.y === wall.y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+resetScoreBtn.addEventListener('click', () => {
+    if (localStorage.getItem("Highscore")) {
+        localStorage.removeItem("Highscore");
+        location.reload();
+    }
 });
 
-
-/*const canvas = document.querySelector("#game-canvas");
-const ctx = canvas.getContext("2d");
-
-let posY = 0;
-let posX = 0;
-ctx.fillRect(posX, posY, 50, 50);
-
-document.addEventListener("keydown", move);
-
-function move(e) {
-    switch (e.key) {
-        case "ArrowLeft":
-            posX -= 50;
-            draw();
-            break;
-        case "ArrowUp":
-            posY -= 50;
-            draw();
-            break;
-        case "ArrowRight":
-            posX += 50;
-            draw();
-            break;
-        case "ArrowDown":
-            posY += 50;
-            draw();
-            break;
-        default: 
-            break;
+function loseCheck() {
+    if (collisionCheck()) {
+        gameOver();
     }
 }
 
-function draw() {
-    if (posY < 0 || posY > 550 || posX < 0 || posY > 550) {
-        alert("Out of bounds");
-        posX = 0;
-        posY = 0;
+function gameOver() {
+    frameSkips = 10
+    snake.x = 0;
+    snake.y = 0;
+    snake.directionX = 0;
+    snake.directionY = 0;
+    snake.bodyX = parseInt(Math.random() * 24) * snake.unit;
+    snake.bodyY = parseInt(Math.random() * 24) * snake.unit;
+    apple.x = parseInt(Math.random() * 24) * snake.unit;
+    apple.y = parseInt(Math.random() * 24) * snake.unit;
+    wall.x = parseInt(Math.random() * 24) * snake.unit;
+    wall.y = parseInt(Math.random() * 24) * snake.unit;
+    wall.bodyX = [];
+    wall.bodyY = [];
+    if (Number(playerScore.innerHTML) > Number(playerHighscore.innerHTML)) {
+        alert("New High Score! ツ");
+    } else {
+        alert("Game Over! o︵o");
     }
-    ctx.clearRect(0, 0, 600, 600);
-    ctx.fillRect(posX, posY, 50, 50);
-}*/
-
+    if (Number(playerScore.innerHTML) > Number(playerHighscore.innerHTML)) {
+        localStorage.setItem("Highscore", playerScore.innerHTML);
+    }
+    if (localStorage.getItem("Highscore")) {
+        playerHighscore.innerHTML = localStorage.getItem("Highscore");
+    }
+    playerScore.innerHTML = 0;
+}
