ad25be6e67eeb3543024179d21ce27a1818c55eb
Tanix <91118560+Tanix98@users.noreply.github.com>
2023-09-27 00:11:25 +0200

    change apples to mangoes, color from green to yellow for improved accessibility

--- readme.md
+++ readme.md
@@ -3,19 +3,21 @@
 ## A simple snake game made using HTML and JavaScript. [Click here to play](https://tanix98.github.io/snake-game/)
 
 ### Potential future features
-- Mobile playability
-- Grey walls with randomized sizes and spawn locations scattered throughout the map. Will result in game over if snake hits them
-- ~~Different colored snake head~~
-- ~~Improved accessibility with aria label, role and colorblind friendly color palette~~
-- Snake able to turn backwards if no apples have been eaten
-- Sound effects
-- Background image
-- Game Over alert replaced with text on canvas element
+
+-   Mobile playability
+-   Grey walls with randomized sizes and spawn locations scattered throughout the map. Will result in game over if snake hits them
+-   ~~Different colored snake head~~
+-   ~~Improved accessibility with aria label, role and colorblind friendly color palette~~
+-   Snake able to turn backwards if no mangoes have been eaten
+-   Sound effects
+-   Background image
+-   Game Over alert replaced with text on canvas element
 
 ### Known issues:
-- Snake body will often increase by two instead of one when eating the first apple, and a green block will show up for 1 second nearby
-- Snake cannot collide with the last part of its body
-- Grey wall only spawns as one block
-- Low chance of snake spawning inside wall
+
+-   Snake body will often increase by two instead of one when eating the first mango, and a green block will show up for 1 second nearby
+-   Snake cannot collide with the last part of its body
+-   Grey wall only spawns as one block
+-   Low chance of snake spawning inside wall
 
 Snake icon by <a href="https://freeicons.io/profile/8939">Melvin ilham Oktaviansyah</a> on <a href="https://freeicons.io">freeicons.io</a>
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const canvas = document.querySelector("#game-canvas");
-const ctx = canvas.getContext("2d");
-const playerScore = document.querySelector("#score-number");
-const playerHighscore = document.querySelector("#highscore-number");
-const resetScoreBtn = document.querySelector("#reset-score-btn");
+const canvas = document.querySelector('#game-canvas');
+const ctx = canvas.getContext('2d');
+const playerScore = document.querySelector('#score-number');
+const playerHighscore = document.querySelector('#highscore-number');
+const resetScoreBtn = document.querySelector('#reset-score-btn');
 //let score = 0
 
-if (localStorage.getItem("Highscore")) {
-    playerHighscore.innerHTML = localStorage.getItem("Highscore");
+if (localStorage.getItem('Highscore')) {
+    playerHighscore.innerHTML = localStorage.getItem('Highscore');
 }
 
 let snake = {
@@ -15,70 +15,70 @@ let snake = {
     y: 0,
     directionX: 0,
     directionY: 0,
-    color: "#73d002",
+    color: '#73d002',
     bodyX: [],
-    bodyY: []
-}
+    bodyY: [],
+};
 
-let apple = {
+let mango = {
     unit: 25,
     x: 0,
     y: 0,
-    color: "#ac1a00"
-}
-apple.x = parseInt(Math.random() * 24) * apple.unit,
-apple.y = parseInt(Math.random() * 24) * apple.unit
+    color: '#FFC107',
+};
+(mango.x = parseInt(Math.random() * 24) * mango.unit),
+    (omango.y = parseInt(Math.random() * 24) * mango.unit);
 
 let wall = {
     unit: 25,
     x: 0,
     y: 0,
-    color: "#c1c1c1",
+    color: '#c1c1c1',
     bodyX: [],
-    bodyY: []
-}
-wall.x = parseInt(Math.random() * 24) * wall.unit,
-wall.y = parseInt(Math.random() * 24) * wall.unit
+    bodyY: [],
+};
+(wall.x = parseInt(Math.random() * 24) * wall.unit),
+    (wall.y = parseInt(Math.random() * 24) * wall.unit);
 
-document.addEventListener("keydown", move);
+document.addEventListener('keydown', move);
 
 function move(e) {
     // Moves snake with arrowkeys
     switch (e.key) {
-        case "ArrowLeft":
-        case "a":
+        case 'ArrowLeft':
+        case 'a':
             if (!snake.directionX) {
                 snake.directionX = -1;
                 snake.directionY = 0;
             }
             e.preventDefault();
-        break;
-        case "ArrowUp":
-        case "w":
+            break;
+        case 'ArrowUp':
+        case 'w':
             if (!snake.directionY) {
                 snake.directionX = 0;
                 snake.directionY = -1;
             }
             e.preventDefault();
-        break;
-        case "ArrowRight":
-        case "d":
+            break;
+        case 'ArrowRight':
+        case 'd':
             if (!snake.directionX) {
                 snake.directionX = 1;
                 snake.directionY = 0;
             }
             e.preventDefault();
-        break;
-        case "ArrowDown":
-        case "s":
+            break;
+        case 'ArrowDown':
+        case 's':
             if (!snake.directionY) {
                 snake.directionX = 0;
                 snake.directionY = 1;
             }
             e.preventDefault();
-        break;
-        default: 
-        break;
+            break;
+        default:
+            break;
     }
 }
 
@@ -97,12 +97,17 @@ function gameLoop() {
 }
 
 function game() {
-    // Check if snake hits apple
+    // Check if snake hits mango
     eat();
-    if (snake.y < 0 || snake.y > 575 || snake.x < 0 || snake.x > 575 || snake.x === wall.x && snake.y === wall.y) {
+    if (
+        snake.y < 0 ||
+        snake.y > 575 ||
+        snake.x < 0 ||
+        snake.x > 575 ||
+        (snake.x === wall.x && snake.y === wall.y)
+    ) {
         gameOver();
-    }
-    else {
+    } else {
         snake.x += snake.directionX * snake.unit;
         snake.y += snake.directionY * snake.unit;
     }
@@ -117,16 +122,16 @@ function draw() {
     // Draw walls
     ctx.fillStyle = wall.color;
     ctx.fillRect(wall.x, wall.y, wall.unit, wall.unit);
-    // Draw apple
-    ctx.fillStyle = apple.color;
-    ctx.fillRect(apple.x, apple.y, apple.unit, apple.unit);
+    // Draw mango
+    ctx.fillStyle = mango.color;
+    ctx.fillRect(mango.x, mango.y, mango.unit, mango.unit);
     // Draw snake head
-    ctx.fillStyle = "#66ba00";
+    ctx.fillStyle = '#66ba00';
     ctx.fillRect(snake.x, snake.y, snake.unit, snake.unit);
     ctx.fillStyle = snake.color;
     // Draw snake body
     for (let i = 0; i < snake.bodyX.length; i++) {
-        ctx.fillRect(snake.bodyX[i], snake.bodyY[i], snake.unit, snake.unit)
+        ctx.fillRect(snake.bodyX[i], snake.bodyY[i], snake.unit, snake.unit);
     }
 }
 
@@ -139,19 +144,19 @@ function draw() {
 }*/
 
 function eat() {
-    // Eat apple
-    if (snake.x === apple.x && snake.y === apple.y) {
+    // Eat mango
+    if (snake.x === mango.x && snake.y === mango.y) {
         /*score++;*/
         playerScore.innerHTML++;
         grow();
         if (frameSkips > 2.5) {
             frameSkips--;
-            console.log("speed");
+            console.log('speed');
         }
-        // Spawn a new apple if spawncheck is true
+        // Spawn a new mango if spawncheck is true
         do {
-            apple.y = parseInt(Math.random() * 24) * apple.unit;
-            apple.x = parseInt(Math.random() * 24) * apple.unit;
+            mango.y = parseInt(Math.random() * 24) * mango.unit;
+            mango.x = parseInt(Math.random() * 24) * mango.unit;
         } while (spawnCheck());
     } else {
         moveBody();
@@ -162,15 +167,15 @@ function grow() {
     // Grow snake body
     snake.bodyX = [snake.x].concat(snake.bodyX);
     snake.bodyY = [snake.y].concat(snake.bodyY);
-    console.log("x: " + snake.bodyX + " " + "y: " + snake.bodyY);
+    console.log('x: ' + snake.bodyX + ' ' + 'y: ' + snake.bodyY);
 }
 
 function moveBody() {
     // Move snake body
     if (snake.bodyX.length > 1) {
         for (let i = snake.bodyX.length - 1; i > 0; i--) {
-            snake.bodyX[i] = snake.bodyX[i-1];
-            snake.bodyY[i] = snake.bodyY[i-1];
+            snake.bodyX[i] = snake.bodyX[i - 1];
+            snake.bodyY[i] = snake.bodyY[i - 1];
         }
     }
     if (snake.bodyX.length > 0) {
@@ -190,9 +195,12 @@ function collisionCheck() {
 }
 
 function spawnCheck() {
-    // Check if the apple spawns in snake body or a wall
+    // Check if the mango spawns in snake body or a wall
     for (let i = 0; i < snake.bodyX.length; i++) {
-        if (snake.bodyX[i] === apple.x && snake.bodyY[i] === apple.y || apple.x === wall.x && apple.y === wall.y) {
+        if (
+            (snake.bodyX[i] === mango.x && snake.bodyY[i] === mango.y) ||
+            (mango.x === wall.x && mango.y === wall.y)
+        ) {
             return true;
         }
     }
@@ -201,36 +209,36 @@ function spawnCheck() {
 
 // Reset highscore button
 resetScoreBtn.addEventListener('click', () => {
-    if (localStorage.getItem("Highscore")) {
-        localStorage.removeItem("Highscore");
+    if (localStorage.getItem('Highscore')) {
+        localStorage.removeItem('Highscore');
         location.reload();
     }
 });
 
 function gameOver() {
-    frameSkips = 10
+    frameSkips = 10;
     snake.x = 0;
     snake.y = 0;
     snake.directionX = 0;
     snake.directionY = 0;
     snake.bodyX = parseInt(Math.random() * 24) * snake.unit;
     snake.bodyY = parseInt(Math.random() * 24) * snake.unit;
-    apple.x = parseInt(Math.random() * 24) * snake.unit;
-    apple.y = parseInt(Math.random() * 24) * snake.unit;
+    mango.x = parseInt(Math.random() * 24) * snake.unit;
+    mango.y = parseInt(Math.random() * 24) * snake.unit;
     wall.x = parseInt(Math.random() * 24) * snake.unit;
     wall.y = parseInt(Math.random() * 24) * snake.unit;
     wall.bodyX = [];
     wall.bodyY = [];
     if (Number(playerScore.innerHTML) > Number(playerHighscore.innerHTML)) {
-        alert("New High Score! ツ");
+        alert('New High Score! ツ');
     } else {
-        alert("Game Over! o︵o");
+        alert('Game Over! o︵o');
     }
     if (Number(playerScore.innerHTML) > Number(playerHighscore.innerHTML)) {
-        localStorage.setItem("Highscore", playerScore.innerHTML);
+        localStorage.setItem('Highscore', playerScore.innerHTML);
     }
-    if (localStorage.getItem("Highscore")) {
-        playerHighscore.innerHTML = localStorage.getItem("Highscore");
+    if (localStorage.getItem('Highscore')) {
+        playerHighscore.innerHTML = localStorage.getItem('Highscore');
     }
     playerScore.innerHTML = 0;
 }
